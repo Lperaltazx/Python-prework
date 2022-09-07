e0710db72102a17a6c295eb4932b95253ceb38b6
Lperaltazx <77282709+Lperaltazx@users.noreply.github.com>
2022-09-07 01:07:24 -0400

    Update prework.py

--- prework.py
+++ prework.py
@@ -1,13 +1,23 @@
+# Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below. def hello_name(user_name):
+
 def hello_name(user_name):
     print("hello_"+user_name)
 user_name = input('Enter USERNAME: ')
 hello_name(user_name)
+
+# Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing def first_odds():
 
 def first_odd():
     for i in range(1,101):
         if i % 2 != 0:
             print(i)
 first_odd()
+
+
+# Question 3
+# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below. def max_num_in_list(a_list):
 
 def max_num_in_list(a_list):
     maximum = a_list[0]
@@ -18,6 +28,8 @@ def max_num_in_list(a_list):
 a_list = [10, 20, 30, 40, 50]
 print(max_num_in_list(a_list))
 
+# Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100 unless it is also divisible by 400. The return should be boolean Type (true/false). def is_leap_year(a_year):
 
 # Function named is_leap_year that takes year as parameter and returns
 # True if the year is leap year otherwise False
@@ -39,6 +51,9 @@ def is_leap_year(a_year):
 a_year = int(input())
 # Calling function is_leap_year
 print(is_leap_year(a_year))
+
+# Question 5
+# Write a function to check to see if all numbers in the list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type. def is_consecutive(a_list):
 
 def is_consecutive(a_list):
     return sorted(a_list) == list(range(min(a_list), max(a_list)+1))
